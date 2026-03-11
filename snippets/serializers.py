43449840d75b7bbe1b923ca34fff17fa2754391e
Andro7572 <a.xachaturiani@icloud.com>
2026-03-12 02:03:23 +0400

    fixed bug

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,9 +6,6 @@ from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
-    highlight = serializers.HyperlinkedIdentityField(
-        view_name="snippet-highlight", format="html"
-    )
 
     class Meta:
         model = Snippet
